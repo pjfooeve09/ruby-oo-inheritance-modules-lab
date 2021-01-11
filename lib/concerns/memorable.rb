d903b747a87980d1ec4f47efd3b0bf56410bb104
Parham Javadi <pjfooeve09@yahoo.com>
2021-01-11 11:40:01 -0800

    commit tree f90216d1ce69b13519cfd1145d25ed4c97b7021d

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,23 @@
+#Remember that the self keyword is omitted when defining class methods inside modules
+
+module Memorable
+    module ClassMethods
+
+    def reset_all
+        self.all.clear
+    end
+
+    def count
+        self.all.count
+    end
+  end
+
+ module InstanceMethods
+    def initialize
+        self.class.all << self
+    end
+  end
+
+end
+
+
